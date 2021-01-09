d3c7f2434e42b7ff753ffedbf8f7996bf4847aee
Anton Burchak <anton_burchak@epam.com>
2021-01-10 00:08:57 +0200

    Removed unnecessary folder

--- spotify-app/src/root/constants/constants.js
+++
@@ -1,2 +0,0 @@
-export const GET_CATEGORIES = 'GET_CATEGORIES';
-export const SET_CATEGORIES = 'SET_CATEGORIES';
--- spotify-app/src/root/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import categoriesReducer from "./reducers/categories.reducer";
-
-const rootReducer = combineReducers({
-    categoriesReducer
-});
-
-export default rootReducer;
-
